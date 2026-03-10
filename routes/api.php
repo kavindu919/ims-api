40320cd985526f0cd1884a2834d14381add579ff
kavindu919 <jayakodyk2@gmail.com>
2026-03-10 13:15:28 +0530

    feat: create the cupboad crud and related api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CupboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,10 @@ Route::middleware('auth:sanctum')->prefix('auth')->group(function () {
     Route::post('/users', [UserController::class, 'createUser'])->name('users.create');
     Route::post('/user/get', [UserController::class, 'getUser'])->name('user-get');
     Route::post('/user/update', [UserController::class, 'updateUser'])->name('users.update');
+
+    Route::get('/cupboards', [CupboardController::class, 'getAllCupboards'])->name('cupboards.getall');
+    Route::post('/cupboards/create', [CupboardController::class, 'createCupboards'])->name('cupboards.create');
+    Route::post('/cupboard/get', [CupboardController::class, 'getCupboardWithPlaces'])->name('cupboard.get');
+    Route::post('/cupboard/update', [CupboardController::class, 'updateCupboard'])->name('cupboard.update');
+    Route::post('/cupboard/delete', [CupboardController::class, 'deleteCupboard'])->name('cupboard.delete');
 });
